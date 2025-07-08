f16daedf40412fd734946333984a19ed7d5f9ef5
afzarnazruddinshah <32347425+afzarnazruddinshah@users.noreply.github.com>
2025-07-08 19:08:10 +0530

    Added body-parser, cors, separated server code

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+
 
 app.get('/getGreeting', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Listening on ${port}`);
-})
+module.exports = app;
