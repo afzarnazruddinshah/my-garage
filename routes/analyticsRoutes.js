12ee63ce6bae572cc293c37fcab8f46db70aae43
afzarnazruddinshah <32347425+afzarnazruddinshah@users.noreply.github.com>
2025-08-20 18:51:10 +0530

    Analytics - remove extra api

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -35,38 +35,6 @@ router.post("/vehicles/most-serviced", async (req, res) => {
     });
 });
 
-router.post("/vehicles/most-revenue", async (req, res) => {
-  let { hasModel, startDate, endDate } = req.query;
-  let modelColumn = "";
-  let groupByModelStatement = "";
-  if (hasModel) {
-    modelColumn = "vehicles.model,";
-    groupByModelStatement = ",vehicles.model";
-  }
-  if (!startDate) {
-    startDate = "2025-01-01";
-  }
-  if (!endDate) {
-    endDate = "2026-01-01";
-  }
-  let selectStatement = `SELECT vehicles.make, ${modelColumn} COUNT(assignments.issue) as service_count`;
-  const dbQuery = `
-    ${selectStatement}
-    FROM vehicles
-    INNER JOIN assignments
-    ON assignments.vin = vehicles.vin
-    WHERE assignments.checkin_date < '${endDate}' AND assignments.checkin_date > '${startDate}'
-    GROUP BY  vehicles.make ${groupByModelStatement}
-    ORDER BY service_count DESC;`;
-  db.any(dbQuery)
-    .then((data) => {
-      res.json({ status: constants.successStatus, data: data });
-    })
-    .catch((error) => {
-      res.json({ error, status: constants.errorStatus });
-    });
-});
-
 router.post("/analytics/most-revenue", async (req, res) => {
   let { startDate, endDate } = req.query;
 
@@ -96,5 +64,6 @@ router.post("/analytics/most-revenue", async (req, res) => {
 });
 
 module.exports = router;
+
 
 
